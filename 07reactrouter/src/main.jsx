c6d09cfa1a3495da0fb0ea228f70eeeb232f94d7
whyhimanshu <vermahim402@gmail.com>
2025-12-11 07:36:38 +0530

    C1

---
+++ 07reactrouter/src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+const router = createBrowserRouter([
+  
+])
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
